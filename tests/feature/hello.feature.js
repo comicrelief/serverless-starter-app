64f860a7c00e63e644861c57acbde62cc0fab66c
Adam Clark <a.clark@comicrelief.com>
2017-11-16 15:00:16 +0000

    Fix tests and add header tests

--- tests/feature/hello.feature.js
+++ tests/feature/hello.feature.js
@@ -17,14 +17,31 @@ describe('HelloAction', () => {
   });
 
   it('Should expect the response to return a successful status', () => {
-    return promisifiedAction().then((response, data, test) => {
+    return promisifiedAction({queryStringParameters : {}}).then((response, data, test) => {
       expect(response.status).to.eql(success);
     });
   });
 
+  it('Should return CORS headers', () => {
+    return promisifiedAction({queryStringParameters : {}}).then((response, data, test) => {
+      const headers = response.headers;
+      expect(headers['Content-Type']).to.eql('application/json');
+      expect(headers['Access-Control-Allow-Origin']).to.eql('*');
+      expect(headers['Access-Control-Allow-Credentials']).to.eql(true);
+    });
+  });
+
   it('Should expect the response to return a response of hello', () => {
-    return promisifiedAction().then((response, data, test) => {
-      expect(response.response.test).to.eql('hello');
+    return promisifiedAction({queryStringParameters : {}}).then((response, data, test) => {
+      const body = JSON.parse(response.body);
+      expect(body.test).to.eql('Hello');
+    });
+  });
+
+  it('Should expect the response to return a customised response when a name is set using the query parameters', () => {
+    return promisifiedAction({queryStringParameters : { name: 'Adam' }}).then((response, data, test) => {
+      const body = JSON.parse(response.body);
+      expect(body.test).to.eql('Hello Adam');
     });
   });
 
