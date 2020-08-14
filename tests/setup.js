17fb398ff7647a66d9ba0b0c63d79b34f4f6bdd0
William Di Pasquale <wdp@dipasqualew.com>
2020-08-14 11:06:38 +0100

    Define `offline-feature-test` script
    While feature tests should normally completely run on AWS,
    during the development of those it is helpful to run them
    against `serverless-offline`. To do so, the API Key and
    the service URL are overriden in `setup.js`.

--- tests/setup.js
+++ tests/setup.js
@@ -8,4 +8,13 @@ before(() => {
 
   // Force chai to show the complete object diff
   chai.config.truncateThreshold = 0;
+
+  // Execute the feature tests
+  // against serverless offline.
+  // Override URL and API Key
+  // To match `yarn offline-feature-test`
+  if (process.env.USE_SERVERLESS_OFFLINE) {
+    process.env.SERVICE_URL = 'http://localhost:3001';
+    process.env.SERVICE_KEY = 'api-key';
+  }
 });
