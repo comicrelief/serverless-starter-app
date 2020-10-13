80a684930c46259b7803d1b28aa53f2c9a39399d
William Di Pasquale <wdp@dipasqualew.com>
2020-10-13 16:12:11 +0100

    tests: Convert tests to Jest
    Closes #36

--- tests/feat/hello.spec.js
+++ tests/feat/hello.spec.js
@@ -1,30 +1,25 @@
-import ServerlessMochaPlugin from 'serverless-mocha-plugin';
-
 import { lambdaPromisifier } from '@/tests/lib/lambda-promisifier';
 import HelloAction from '@/src/Action/Hello.action';
 
-const {
-  chai: { expect },
-} = ServerlessMochaPlugin;
 const promisifiedAction = lambdaPromisifier(HelloAction);
 
 // Test definitions.
-describe('HelloAction', function () {
+describe('feat.Hello', function () {
   it('Should expect the response to return a successful status', async () => {
     const response = await promisifiedAction({ queryStringParameters: {} });
-    expect(response.statusCode).to.eql(200);
+    expect(response.statusCode).toEqual(200);
   });
 
   it('Should return CORS headers', async () => {
     const { headers } = await promisifiedAction({ queryStringParameters: {} });
-    expect(headers['Content-Type']).to.eql('application/json');
-    expect(headers['Access-Control-Allow-Origin']).to.eql('*');
-    expect(headers['Access-Control-Allow-Credentials']).to.eql(true);
+    expect(headers['Content-Type']).toEqual('application/json');
+    expect(headers['Access-Control-Allow-Origin']).toEqual('*');
+    expect(headers['Access-Control-Allow-Credentials']).toEqual(true);
   });
 
   it('Should expect the response to return a response of hello', async () => {
     const response = await promisifiedAction({ queryStringParameters: {} });
     const body = JSON.parse(response.body);
-    expect(body.data.response).to.eql('Hello');
+    expect(body.data.response).toEqual('Hello');
   });
 });
