7908d415f558d9997537b14203a4e59e538e365c
corinja <c.ashwell@comicelief.com>
2020-11-04 16:03:05 +0000

    Added urlToOptions suggested at https://runkit.com/szmarczak/5e1a23d8e4d193001a709110 from https://github.com/sindresorhus/got/issues/876 still not working

--- src/Common/data-processor-got.js
+++ src/Common/data-processor-got.js
@@ -1,8 +1,20 @@
 import * as got from 'got';
+import { HttpsProxyAgent } from 'hpagent';
+import * as https from 'https';
+import * as http from 'http';
+
+const urlToOptions = require('got/dist/source/utils/url-to-options').default;
+const instance = got.extend({
+  request: (url, options, callback) => {
+    if(url.protocol === 'https') {
+      return https.request({...options, ...urlToOptions(url)}, callback);
+    }
+    return http.request({...options, ...urlToOptions(url)}, callback);
+  }
+});
 
 export default class DataProcessorGot {
-
-  async getWebData() {
+   async getWebData() {
     console.log('Getting data');
     let response = await this.getContent('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
     return response;
@@ -14,13 +26,26 @@ export default class DataProcessorGot {
 
   async getContent(url) {
     try {
-      const response = await got(url, { JSON: true });
+      const response = await instance(url, { 
+        // agent: {
+        //   https: new HttpsProxyAgent({
+        //     keepAlive: true,
+        //     keepAliveMsecs: 1000,
+        //     maxSockets: 256,
+        //     maxFreeSocket: 256,
+        //     scheduling: 'lifo',
+        //     proxy: 'http://ipv4.86.161.174.79.webdefence.global.blackspider.com:8080'
+        //   })
+        // },
+        JSON: true,
+        // followRedirect: false,
+      });
       return JSON.parse(response.body);
     } catch (error) {
       console.log('Caught error in DataProcessorGot');
-      // console.log(error.options);
+      console.log(error.options);
       console.log('Here is the RequestError:');
-      //console.log(JSON.parse(error).RequestError);
+      console.log(error.RequestError);
       console.log('Here is the full Error object');
       console.log(error);
       throw error;
