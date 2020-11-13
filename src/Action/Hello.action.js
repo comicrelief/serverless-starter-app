0475915c96fb6569d4f060ae13c9a8c704b75617
William Di Pasquale <wdp@dipasqualew.com>
2020-11-13 10:35:48 +0000

    feat: Enable JSdoc plugin for `serverless-starter-app`
    - The JSdoc plugin is activated
    - The codebase lints successfully
    
    See:
    - [ENG-33]
    - [ENG-44]

--- src/Action/Hello.action.js
+++ src/Action/Hello.action.js
@@ -1,8 +1,19 @@
-import { LambdaWrapper, DependencyInjection, ResponseModel, RequestService } from '@comicrelief/lambda-wrapper';
+import {
+  DependencyInjection,
+  LambdaWrapper,
+  ResponseModel,
+  RequestService,
+} from '@comicrelief/lambda-wrapper';
 
 import CONFIGURATION from '@/src/Config/Configuration';
 
-export default LambdaWrapper(CONFIGURATION, async (di: DependencyInjection, request: RequestService) => {
+/**
+ * An example handler
+ *
+ * @param {DependencyInjection} di
+ * @param {RequestService} request
+ */
+export const Hello = async (di: DependencyInjection, request: RequestService) => {
   // Get a name from the query parameters.
   const name = request.get('name');
 
@@ -10,4 +21,6 @@ export default LambdaWrapper(CONFIGURATION, async (di: DependencyInjection, requ
     response: name !== null ? `Hello ${name}` : 'Hello',
   };
   return ResponseModel.generate(body, 200, 'ok');
-});
+};
+
+export default LambdaWrapper(CONFIGURATION, Hello);
