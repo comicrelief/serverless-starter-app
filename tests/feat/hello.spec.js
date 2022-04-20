75ac279b52bc4fc87ddd4cb40bf1e65f2479667f
mohamed <mohamed.mahmoud@camelcasetech.com>
2022-04-20 15:34:44 +0200

    Rewrite tests to work with serverless-offline instead of running Lambda code in-process (delete lambda-promisifier)

--- tests/feat/hello.spec.js
+++ tests/feat/hello.spec.js
@@ -1,26 +1,25 @@
-import { lambdaPromisifier } from '@/tests/lib/lambda-promisifier';
 import HelloAction from '@/src/Action/Hello.action';
-
-const promisifiedAction = lambdaPromisifier(HelloAction);
+import { getRequest } from '../lib/shared';
 
 // Test definitions.
 describe('feat.Hello', () => {
-  const event = { queryStringParameters: {} };
-
   it('should return a successful status', async () => {
-    const response = await promisifiedAction(event);
+    const request = getRequest();
+    const response = await HelloAction({}, request);
     expect(response.statusCode).toEqual(200);
   });
 
   it('should return CORS headers', async () => {
-    const { headers } = await promisifiedAction(event);
+    const request = getRequest();
+    const { headers } = await HelloAction({}, request);
     expect(headers['Content-Type']).toEqual('application/json');
     expect(headers['Access-Control-Allow-Origin']).toEqual('*');
     expect(headers['Access-Control-Allow-Credentials']).toEqual(true);
   });
 
   it('should respond with "Hello"', async () => {
-    const response = await promisifiedAction(event);
+    const request = getRequest();
+    const response = await HelloAction({}, request);
     const body = JSON.parse(response.body);
     expect(body.data.response).toEqual('Hello');
   });
