079c2bd308598c3ddb2a2753dd21798cf724a126
corinja <c.ashwell@comicrelief.com>
2023-04-20 12:29:08 +0100

    Move request to test & remove test lib

--- tests/feat/hello.spec.js
+++ tests/feat/hello.spec.js
@@ -1,14 +1,22 @@
-import HelloAction from '../lib/actions/hello';
+import axios from 'axios';
+
+async function makeRequest() {
+  return axios.get('hello', {
+    headers: { Origin: '*' },
+    baseURL: process.env.SERVICE_BASE_URL,
+    responseType: 'json',
+  })
+}
 
 // Test definitions.
 describe('feat.Hello', () => {
   it('should return a successful status', async () => {
-    const response = await HelloAction.hello();
+    const response = await makeRequest();
     expect(response.status).toEqual(200);
   });
 
   it('should return CORS headers', async () => {
-    const { headers } = await HelloAction.hello();
+    const { headers } = await makeRequest();
     const lowercaseheaders = Object.fromEntries(
       Object.entries(headers).map(([k,v]) => [k.toLowerCase(), v])
     );
@@ -18,7 +26,7 @@ describe('feat.Hello', () => {
   });
 
   it('should respond with "Hello"', async () => {
-    const { data } = await HelloAction.hello();
+    const { data } = await makeRequest();
     expect(data.data.response).toEqual('Hello');
   });
 });
