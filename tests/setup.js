88cdb33a690814f2496ab0ece03368d026ef8762
Mohamed Mahmoud <mohamedmahmoudphp@gmail.com>
2023-04-21 18:24:12 +0200

    test: Make HTTP requests instead of calling endpoints in-process (#59)
    * Rewrite tests to work with serverless-offline instead of running Lambda code in-process (delete lambda-promisifier)
    
    * implement 'getRequest' function
    
    * use axios to call serverless-offline
    
    * add axios - fix lint
    
    * Change STAGING_BASE_URL to BASE_URL
    
    * Modify tests:
    - Pass origin header in Axios request
    - Change case of response headers
    
    * Lint fixes
    
    * Renamed URL variable
    
    * Removed unused components
    
    * Upgrade axios & jest
    
    * Improve tests:
    - content/type value & key case response differs between AWS & sls-offline
    
    * Move request to test & remove test lib
    
    * Lint fixes
    
    * Axios lowercases headers so removed this
    
    * Remove outdated comment
    
    * Add caret to axios version to accept non-breaking updates
    
    ---------
    
    Co-authored-by: corinja <c.ashwell@comicrelief.com>

--- tests/setup.js
+++ tests/setup.js
@@ -4,10 +4,5 @@ import { config } from 'dotenv';
 config();
 
 if (process.env.USE_SERVERLESS_OFFLINE) {
-  // Execute the feature tests
-  // against serverless offline.
-  // Override URL and API Key
-  // To match `yarn offline-feature-test`
-  process.env.SERVICE_URL = 'http://localhost:3001';
-  process.env.SERVICE_KEY = 'api-key';
+  process.env.SERVICE_BASE_URL = 'http://localhost:3001';
 }
